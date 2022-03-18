28b9fa78056d963c03994262d52451945a77821a
고준호 <rhwnsgh2@naver.com>
2022-03-19 02:09:03 +0900

    add navigationBar folder create

---
+++ src/components/navigationBar/navBar.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { NavigationBar } from './navBar';
+
+test('renders learn react link', () => {
+  render(<NavigationBar />);
+});
