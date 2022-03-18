d27dd4db36ecd8ad1b7e12c1afe8e37cf2930933
고준호 <rhwnsgh2@naver.com>
2022-03-19 02:54:03 +0900

    sideBar to Function

--- src/components/navigationBar/navBar.tsx
+++ src/components/navigationBar/navBar.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { SideBarData } from './navBarData';
 
 export function NavigationBar() {
+  return <div>{SideBarList()}</div>;
+}
+
+export function SideBarList() {
   const sideBarData = SideBarData.slice();
   const sideBar = sideBarData.map((data) => {
-    return <div>{data.title}</div>;
+    return <div key={data.title}>{data.title}</div>;
   });
-  return <div>{sideBar}</div>;
+  return sideBar;
 }
