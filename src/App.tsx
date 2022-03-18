89467cfe51cce340586e446a8721586a271a3e29
고준호 <rhwnsgh2@naver.com>
2022-03-19 03:09:23 +0900

    add react-icon, homePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { NavigationBar } from './components/navigationBar/navBar';
+import { Home } from './pages/home';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
-      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/react" element={<Home />} />
+      </Routes>
     </BrowserRouter>
   );
 }
