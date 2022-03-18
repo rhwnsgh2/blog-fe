751d1cafe5a430fa266598313d48332f55b0b2fe
고준호 <rhwnsgh2@naver.com>
2022-03-18 16:04:43 +0900

    setting: eslint, prettier add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      해치웠나?
+      <div>해치웠나?a</div>
     </div>
   );
 }
