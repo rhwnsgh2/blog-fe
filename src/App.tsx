65e10b052b8690b308d1896f01f78bce6ba32286
고준호 <rhwnsgh2@naver.com>
2022-03-19 02:51:51 +0900

    render sideBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import { NavigationBar } from './components/navBar';
+import { NavigationBar } from './components/navigationBar/navBar';
 
 function App(): JSX.Element {
   return (
--- src/components/navigationBar/navBar.tsx
+++ src/components/navigationBar/navBar.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { SideBarData } from './navBarData';
 
 export function NavigationBar() {
-  const sideBarData = { ...SideBarData };
-  sideBarData.map((data) => {
+  const sideBarData = SideBarData.slice();
+  const sideBar = sideBarData.map((data) => {
     return <div>{data.title}</div>;
   });
-  return <div>{sideBarData}</div>;
+  return <div>{sideBar}</div>;
 }
