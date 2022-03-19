99cdded684470f6bbd518346fb9234ba3fd66a15
고준호 <rhwnsgh2@naver.com>
2022-03-19 16:00:45 +0900

    merge naviBar

---
+++ src/pages/flutter.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { NavigationBarContainer } from '../components/navigationBar/navBar';
+
+export function FlutterPage(): JSX.Element {
+  return <div>FlutterPage</div>;
+}
