04df314056aebd7e9aa3cd893f2e0471a29bffc7
고준호 <rhwnsgh2@naver.com>
2022-03-19 15:29:04 +0900

    change location - call NavigtationBarContainer to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/home';
+import { routerData } from './components/navigationBar/navBarData';
+import { NavigationBarContainer } from './components/navigationBar/navBar';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
+      <NavigationBarContainer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/react" element={<Home />} />
@@ -14,4 +17,10 @@ function App(): JSX.Element {
   );
 }
 
+function RouteElement(): JSX.Element[] {
+  const ret = routerData.map((item) => {
+    return <Route path={item.path} element />;
+  });
+  return ret;
+}
 export default App;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,9 +2,5 @@ import React from 'react';
 import { NavigationBarContainer } from '../components/navigationBar/navBar';
 
 export function Home(): JSX.Element {
-  return (
-    <div>
-      <NavigationBarContainer /> HOMEPAGE
-    </div>
-  );
+  return <div>HOMEPAGE</div>;
 }
