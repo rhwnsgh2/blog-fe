b9e37b068fbc4aa569681ded61e3940792929149
고준호 <rhwnsgh2@naver.com>
2022-03-20 14:22:36 +0900

    add: react router container type jsxelement

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routerData } from '../components/navigationBar/navBarData';
 
-export const RouteContainer = () => {
+export const RouteContainer = (): JSX.Element => {
   return <Routes>{RouteElement()}</Routes>;
 };
 
