29307a82cf59359f18013a91cb2ff495647ee6cd
고준호 <rhwnsgh2@naver.com>
2022-03-25 19:05:55 +0900

    storybook add

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,20 +7,34 @@ module.exports = {
     'plugin:import/warnings',
     'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
   ],
   rules: {
     'linebreak-style': 0,
     'import/prefer-default-export': 0,
-    'prettier/prettier': ['error', { endofLine: 'auto' }],
+    'prettier/prettier': [
+      'error',
+      {
+        endofLine: 'auto',
+      },
+    ],
     'import/extensions': 0,
     'no-use-before-define': 0,
     'import/no-unresolved': 0,
-    'import/no-extraneous-dependencies': 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
+    'import/no-extraneous-dependencies': 0,
+    // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
     'no-shadow': 0,
     'react/prop-types': 0,
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
     'jsx-a11y/no-noninteractive-element-interactions': 0,
     'no-underscore-dangle': 0,
     'react/function-component-definition': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/require-default-props': 0,
   },
 };
