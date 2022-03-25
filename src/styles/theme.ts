eef150dee985353bc5b32b4d22b2f49e7944506d
고준호 <rhwnsgh2@naver.com>
2022-03-25 12:34:19 +0900

    style component test

---
+++ src/styles/theme.ts
@@ -0,0 +1,16 @@
+// theme.js
+import styled, { css, DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  mainBackground: `rgba(0, 0, 0, 0.85)`,
+  // neutral color
+  title: `rgba(0, 0, 0, 0.85)`,
+  primaryText: `rgba(0, 0, 0, 0.75)`,
+  secondaryText: `rgba(0, 0, 0, 0.45)`,
+  disable: `rgba(0, 0, 0, 0.25)`,
+  border: `rgba(0, 0, 0, 0.15)`,
+  divider: `rgba(0, 0, 0, 0.06)`,
+  background: `rgba(0, 0, 0, 0.04)`,
+  tableHeader: `rgba(0, 0, 0, 0.02)`,
+  // point-color
+};
