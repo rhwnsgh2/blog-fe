023a1bb7b9608d3f6224fcb556c589ddf6de0605
고준호 <rhwnsgh2@naver.com>
2022-03-26 14:41:00 +0900

    add redux, theme reducer

---
+++ src/redux/slice/theme.ts
@@ -0,0 +1,14 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { DefaultTheme } from 'styled-components';
+import { theme } from '../../styles/theme';
+
+export const mainTheme = createSlice({
+  name: 'mainTheme',
+  initialState: theme.lightTheme,
+  reducers: {
+    changeTheme(state, action: PayloadAction<DefaultTheme>) {
+      console.log(action);
+      return action.payload;
+    },
+  },
+});
