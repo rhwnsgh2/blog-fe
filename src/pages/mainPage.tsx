cc0b672f2ca8c6c5d7ae55cbb3bdb3cd06f91c1e
junho <rhwngsh2@naver.com>
2022-05-20 21:54:29 +0900

    fix : MainHeader split with mainPage

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,31 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import MainHeader from '../components/MainHeader';
 
 const MainPage = () => {
-  return (
-    <HeaderWrapper>
-      <TitleWrapper>JH LABS</TitleWrapper>
-    </HeaderWrapper>
-  );
+  return <MainHeader />;
 };
-
-const TitleWrapper = styled.div`
-  height: 44px;
-  margin-left: 228px;
-  position: absolute;
-
-  font-weight: 700;
-  text-align: center;
-  line-height: 44px;
-  font-size: 32px;
-
-  overflow-x: visible;
-`;
-
-const HeaderWrapper = styled.div`
-  height: 80px;
-  width: 100vw;
-  display: flex;
-  align-items: center;
-`;
 export default MainPage;
